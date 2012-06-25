721face84e57f54500cb1a8b9348d4b484083edd
Daniel Snider <qtipkk@gmail.com>
2012-06-25 11:01:24 -0500

    Shut pylint up about django's classonlymethod decorator.

--- survey/urls.py
+++ survey/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from survey.views import *
 
-# pylint: disable-msg=C0103
+# pylint: disable-msg=C0103,E1120
 urlpatterns = patterns('survey.views',
                        url(r'^$', IndexView.as_view(), name='index'),
                        )
