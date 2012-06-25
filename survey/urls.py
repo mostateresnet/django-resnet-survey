913b4f0c12fb91479a59671d8775df1cb9973eb8
Shane <shaneallgeier@gmail.com>
2012-06-25 11:46:15 -0500

    Fixed remaining pylint warnings

--- survey/urls.py
+++ survey/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from survey.views import *
+from survey.views import IndexView
 
 # pylint: disable-msg=C0103,E1120
 urlpatterns = patterns('survey.views',
