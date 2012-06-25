5ab7c5d695cf006241acd4bcb9b4c27be6d71c35
Daniel Snider <qtipkk@gmail.com>
2012-06-25 17:06:07 -0500

    Switched IndexView to a ListView from a TemplateView. Included all surveys in the template context.

--- survey/views.py
+++ survey/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from survey.models import Survey
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    context_object_name = 'surveys'
     template_name = 'survey/index.html'
+
+    def get_queryset(self):
+        return Survey.objects.all()
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
