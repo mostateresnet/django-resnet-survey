65cda902efc395d24de8f581946bbf922be204ad
Daniel Snider <qtipkk@gmail.com>
2012-06-25 12:38:03 -0500

    Created test for IndexView.

--- survey/tests.py
+++ survey/tests.py
@@ -6,11 +6,14 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class IndexViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('admin', email="a@a.com", password='asdf')
+        self.client.login(username='admin', password='asdf')
+
+    def test_get_index(self):
+        response = self.client.get('', follow=True)
+        self.assertEqual(response.status_code, 200)
