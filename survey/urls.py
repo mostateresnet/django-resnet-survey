bcd9e694d8f630bcd6e482a365c8433441478a5c
Shane <shaneallgeier@gmail.com>
2012-06-25 15:04:06 -0500

    Fixed ugly indentation

--- survey/urls.py
+++ survey/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from survey.views import IndexView
 
 # pylint: disable-msg=C0103,E1120
-urlpatterns = patterns('survey.views',
-                       url(r'^$', IndexView.as_view(), name='index'),
-                       )
+urlpatterns = patterns(
+    'survey.views',
+    url(r'^$', IndexView.as_view(), name='index'),
+)
