698c63b93e0403b4bd845e0d6f501918c4ea09ec
Seth Thoenen <seth_thoenen@hotmail.com>
2012-06-26 10:13:29 -0500

    Created inlines and model admins.

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from survey.models import Survey, Question, Choice, Answer
 
-admin.site.register(Survey)
-admin.site.register(Question)
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 0
+    
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+    
+class SurveyAdmin(admin.ModelAdmin):
+    inlines=[
+        QuestionInline,
+    ]
+    
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[
+        ChoiceInline,
+    ]
+    
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Answer)
