db8424dc19e2e968955defc5e72ed62a221bbaa2
Leto Atreides <thedevilsjester@gmail.com>
2012-11-14 11:55:49 -0600

    Tiny fix for Views

--- survey/views.py
+++ survey/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.db import transaction, IntegrityError
 from datetime import timedelta
 from survey import settings
