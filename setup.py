d43bf57b45b827ec908c99b637b0d1d011654ff3
Shane Allgeier <shaneallgeier@gmail.com>
2013-02-20 09:56:56 -0600

    Switched to MIT license

--- setup.py
+++ setup.py
@@ -2,30 +2,30 @@ import os
 import sys
 from setuptools import setup, find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="django-resnet-survey",
-    version="0.1",
-    description="Very simple survey app made with django",
-    long_description=read("README.md"),
-    author="ResNet, Missouri State University",
-    author_email="resnet@missouristate.edu",
-    license="BSD",
-    url="https://github.com/mostateresnet/django-resnet-survey",
+    name='django-resnet-survey',
+    version='0.1',
+    description='Very simple survey app made with django',
+    long_description=read('README.md'),
+    author='ResNet, Missouri State University',
+    author_email='resnet@missouristate.edu',
+    license='MIT',
+    url='https://github.com/mostateresnet/django-resnet-survey',
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 2.5',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        "Framework :: Django",
+        'Framework :: Django',
     ],
     zip_safe=False,
     install_requires=[
@@ -38,4 +38,3 @@ setup(
         'versiontools >= 1.6',
     ],
 )
-
