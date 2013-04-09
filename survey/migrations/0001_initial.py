5905aa91dbed16acf10e3cd59c86f6ae99d443ca
Shane Allgeier <shaneallgeier@gmail.com>
2013-04-09 16:32:10 -0500

    (MIGRATION) Completely reset the migrations for Django 1.3.x compatibility
    To migrate without losing data:
     - git reset --hard f22a10cff3
     - ./manage.py migrate survey
     - git reset --hard HEAD@{1}
     - ./manage.py migrate survey 0001 --fake --delete-ghost-migrations
     - ./manage.py migrate survey

--- survey/migrations/0001_initial.py
+++ survey/migrations/0001_initial.py
@@ -17,6 +17,8 @@ class Migration(SchemaMigration):
             ('end_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
             ('description', self.gf('django.db.models.fields.CharField')(max_length=1024, blank=True)),
             ('use_cookies', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', to=orm['auth.User'])),
+            ('show_social', self.gf('django.db.models.fields.BooleanField')(default=True)),
         ))
         db.send_create_signal('survey', ['Survey'])
 
@@ -44,6 +46,7 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('question', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['survey.Question'])),
             ('message', self.gf('django.db.models.fields.CharField')(max_length=1024)),
+            ('order_number', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
         ))
         db.send_create_signal('survey', ['Choice'])
 
@@ -58,11 +61,26 @@ class Migration(SchemaMigration):
         # Adding model 'Ballot'
         db.create_table('survey_ballot', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('ip', self.gf('django.db.models.fields.GenericIPAddressField')(default='127.0.0.1', max_length=39)),
+            ('ip', self.gf('django.db.models.fields.IPAddressField')(default='127.0.0.1', max_length=15)),
             ('datetime', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ('survey', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['survey.Survey'], null=True)),
         ))
         db.send_create_signal('survey', ['Ballot'])
+
+        # Adding model 'Preset'
+        db.create_table('survey_preset', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=1024)),
+        ))
+        db.send_create_signal('survey', ['Preset'])
+
+        # Adding model 'PresetChoice'
+        db.create_table('survey_presetchoice', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('option', self.gf('django.db.models.fields.CharField')(max_length=1024)),
+            ('preset', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['survey.Preset'])),
+        ))
+        db.send_create_signal('survey', ['PresetChoice'])
 
 
     def backwards(self, orm):
@@ -84,8 +102,50 @@ class Migration(SchemaMigration):
         # Deleting model 'Ballot'
         db.delete_table('survey_ballot')
 
+        # Deleting model 'Preset'
+        db.delete_table('survey_preset')
+
+        # Deleting model 'PresetChoice'
+        db.delete_table('survey_presetchoice')
+
 
     models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         'survey.answer': {
             'Meta': {'object_name': 'Answer'},
             'ballot': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['survey.Ballot']", 'null': 'True'}),
@@ -96,14 +156,26 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Ballot'},
             'datetime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'ip': ('django.db.models.fields.GenericIPAddressField', [], {'default': "'127.0.0.1'", 'max_length': '39'}),
+            'ip': ('django.db.models.fields.IPAddressField', [], {'default': "'127.0.0.1'", 'max_length': '15'}),
             'survey': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['survey.Survey']", 'null': 'True'})
         },
         'survey.choice': {
-            'Meta': {'object_name': 'Choice'},
+            'Meta': {'ordering': "['order_number']", 'object_name': 'Choice'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'message': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'order_number': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
             'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['survey.Question']"})
+        },
+        'survey.preset': {
+            'Meta': {'object_name': 'Preset'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '1024'})
+        },
+        'survey.presetchoice': {
+            'Meta': {'object_name': 'PresetChoice'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'option': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'preset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['survey.Preset']"})
         },
         'survey.question': {
             'Meta': {'ordering': "['order_number']", 'object_name': 'Question'},
@@ -122,9 +194,11 @@ class Migration(SchemaMigration):
         },
         'survey.survey': {
             'Meta': {'object_name': 'Survey'},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'+'", 'to': "orm['auth.User']"}),
             'description': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'blank': 'True'}),
             'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'show_social': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '1024'}),
             'start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
